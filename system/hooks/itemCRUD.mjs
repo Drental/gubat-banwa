5dcf010fb9a8f94b3c8b1f5b743afe4e847869a3
Gunnar Busch <gunnar.busch@googlemail.com>
2023-03-31 21:09:00 +0200

    fix itemCRUD

--- system/hooks/itemCRUD.mjs
+++ system/hooks/itemCRUD.mjs
@@ -12,15 +12,15 @@ export function itemDelete(itemData, options, userId) {
 
 function updateItem(itemData, deleted = false) {
   if (itemData.type === "discipline") {
-    if (deleted) CONFIG.SYSTEM.Discipline_DOCUMENTS.delete(itemData._id);
+    if (deleted) CONFIG.SYSTEM.DISCIPLINE_DOCUMENTS.delete(itemData._id);
     else CONFIG.SYSTEM.DISCIPLINE_DOCUMENTS.set(itemData._id, itemData);
   }
   if (itemData.type === "technique") {
-    if (deleted) CONFIG.SYSTEM.Technique_DOCUMENTS.delete(itemData._id);
+    if (deleted) CONFIG.SYSTEM.TECHNIQUE_DOCUMENTS.delete(itemData._id);
     else CONFIG.SYSTEM.TECHNIQUE_DOCUMENTS.set(itemData._id, itemData);
   }
   if (itemData.type === "antingAnting") {
-    if (deleted) CONFIG.SYSTEM.AntingAnting_DOCUMENTS.delete(itemData._id);
+    if (deleted) CONFIG.SYSTEM.ANTINGANTING_DOCUMENTS.delete(itemData._id);
     else CONFIG.SYSTEM.ANTINGANTING_DOCUMENTS.set(itemData._id, itemData);
   }
 }
